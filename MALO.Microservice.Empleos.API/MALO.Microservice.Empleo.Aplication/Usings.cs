f55ad651f20be25672602f8b9f0ac5c32c819cb1
CHARLY\aguiv <aguivega123@hotmail.com>
2024-10-04 17:47:18 -0600

    .

---
+++ MALO.Microservice.Empleos.API/MALO.Microservice.Empleo.Aplication/Usings.cs
@@ -0,0 +1,3 @@
+﻿global using MALO.Microservice.Empleos.Domain.Interfaces;
+global using MALO.Microservice.Empleos.Domain.Interfaces.Services;
+
---
+++ MALO.Microservice.Empleos.API/MALO.Microservice.Empleos.Infraestructure/Usings.cs
@@ -0,0 +1,9 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using MALO.Microservice.Empleos.Domain.DTOs;
+global using MALO.Microservice.Empleos.Domain.DTOs.Usuario;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using MALO.Microservice.Empleos.Aplication.Commons;
+global using MALO.Microservice.Empleos.Infraestructure.DataContexts;
+global using System.Data;
+global using Microsoft.Data.SqlClient;
